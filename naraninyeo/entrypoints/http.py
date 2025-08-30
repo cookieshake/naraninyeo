618b2e8297f1919d927c74a2528da9b108d5dba5
Youngchan Kim <19236401+cookieshake@users.noreply.github.com>
2025-08-30 17:37:37 +0900

    Change kn commant (#20)
    * feat: add FastAPI application with message handling and Knative deployment steps
    
    * refactor: simplify Knative deployment logic by using SHA tags and consolidating branch handling
    
    * style: format code for improved readability by adding blank lines
    
    * fix: update Knative deployment namespace to 'knative-service'

--- naraninyeo/entrypoints/http.py
+++ naraninyeo/entrypoints/http.py
@@ -11,25 +11,30 @@ from naraninyeo.infrastructure.settings import Settings
 
 app = FastAPI()
 
+
 async def get_message_handler() -> NewMessageHandler:
     return await container.get(NewMessageHandler)
+
 
 async def get_api_client() -> APIClient:
     settings = await container.get(Settings)
     return APIClient(settings)
 
+
 @app.get("/")
 async def read_root():
     return {"Hello": "World"}
+
 
 @app.get("/handle_new_message")
 async def handle_new_message(
     new_message: Message,
     message_handler: Annotated[NewMessageHandler, Depends(get_message_handler)],
-    api_client: Annotated[APIClient, Depends(get_api_client)]
+    api_client: Annotated[APIClient, Depends(get_api_client)],
 ):
     async for reply in message_handler.handle(new_message):
         await api_client.send_response(reply)
+
 
 async def main():
     uvicorn.run(app, host="0.0.0.0", port=8000)
